ef7efd363e3cf55b8daf550e3cd768951dfc6451
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-05-05 19:33:03 +0100

    first of thre in mini-challenges-3

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,19 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    let newText = []
+    let count = 0
+    for (let i = 0; i < text.length; i++) {
+        newText.push(text[i])
+        if (text[i] == text[i + 1]) {
+            count++
+        }
+        else {
+            count = 0
+        }
+        if (count === 2) {
+            newText.push("[")
+        }
+    }
+    return newText
+}
 
 module.exports = isolateDuplicates;
